9880b9358623a27a65e1570f81e672896f1ca17d
Stephen.Carmody <Stephen.Carmody@wolterskluwer.com>
2021-04-27 19:18:20 +1000

    asdf

--- example_client.js
+++ example_client.js
@@ -259,7 +259,9 @@
 
 	window.Chat = Chat;
 
-	const chat = new Chat( "ws://localhost:8500" );
+	//const chat = new Chat( "ws://localhost:8500" );
+	console.log( `ws://${location.hostname}:8500` );
+	const chat = new Chat( `ws://jewel-band-verbena.glitch.me:8500` );
 	document.body.append( chat.dom );
 
 
